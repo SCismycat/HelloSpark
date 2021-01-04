75e1e1d342562f44e8dea3dd8c792ac77d7ae0ef
Leslee <leelovesc@gmail.com>
2021-01-04 23:00:48 +0800

    First Commit

---
+++ src/main/scala/GitbhubAnly.scala
@@ -0,0 +1,36 @@
+import org.apache.spark.sql.SparkSession
+
+
+object GitbhubAnly {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("github push analyse")
+      .master("local[*]")
+      .getOrCreate()
+    val sc = spark.sparkContext
+
+    val homeDir = "E:\\IdeaProject\\hellospark\\src\\main\\resources\\data\\"
+    val inputPath = homeDir + "2015-03-01-0.json"
+    val ghLog = spark.read.json(inputPath)
+    val pushes = ghLog.filter("type='PushEvent'")
+    pushes.printSchema()
+    println("all event:" + ghLog.count())
+    println("only pushes" + pushes.count())
+    pushes.show(5)
+
+    //数据汇总
+    print("数据汇总..")
+    val grouped = pushes.groupBy("actor.login").count
+    grouped.show(5)
+
+    //输出并排序
+    val ordered = grouped.orderBy(grouped("count").desc) //等同于调用了apply
+    ordered.show(5)
+
+
+
+
+  }
+}
+
+
---
+++ src/main/scala/testDemo.scala
@@ -0,0 +1,52 @@
+import org.apache.spark
+import org.apache.spark.ml.fpm.FPGrowth
+import org.apache.spark.sql.SparkSession
+
+object testDemo {
+
+  def main(args:Array[String]){
+    val spark = SparkSession
+      .builder
+      .appName("FPGrowthDemo").master("local")
+      .config("spark.sql.warehouse.dir", "C:\\study\\sparktest")
+      .getOrCreate()
+
+    import spark.implicits._
+    val dataset = spark.createDataset(Seq(
+      "D E",
+      "D E",
+      "A B C",
+      "A B C E",
+      "B E",
+      "C D E",
+      "A B C",
+      "A B C E",
+      "B E",
+      "F G",
+      "D F")
+    ).map(t => t.split(" ")).toDF("items")
+    println(dataset.show(5))
+
+    val fpgrowth = new FPGrowth().setItemsCol("items").setMinSupport(0.3)
+    //val fpgrowth = new FPGrowth().setItemsCol("items").setMinSupport(0.3) .setMinConfidence(0.6)
+    val model = fpgrowth.fit(dataset)
+
+    // Display frequent itemsets.
+    model.freqItemsets.show()
+
+    // Display generated association rules.
+    model.associationRules.show()
+
+    // transform examines the input items against all the association rules and summarize the
+    // consequents as prediction
+    model.transform(dataset).show()
+
+    //save model
+    model.save("/tmp/fp.ml")
+
+    //load saved model
+    import org.apache.spark.ml.fpm.FPGrowthModel
+    val savedModel = FPGrowthModel.load("/tmp/fp.ml")
+  }
+
+}
